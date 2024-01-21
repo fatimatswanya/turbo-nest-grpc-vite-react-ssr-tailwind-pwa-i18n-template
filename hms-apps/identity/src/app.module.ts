31dc17f7d4f785e6325f7f9412729cd80f1a9eba
gracetswanya <gracetswanya@gmail.com>
2024-01-21 23:10:00 +0100

    updated assignment

--- hms-apps/identity/src/app.module.ts
+++ hms-apps/identity/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'hms',
+    synchronize: true,
+    autoLoadEntities: true
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
